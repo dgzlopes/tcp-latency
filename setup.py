39d84e4ff6a52f4b2fc36a2a7c0a394d2f04ca8b
dgzlopes <danielgonzalezlopes@gmail.com>
2019-07-19 07:35:45 +0200

    Small version bump
    Signed-off-by: dgzlopes <danielgonzalezlopes@gmail.com>

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open('README.md') as f:
 
 setup(
     name='tcp-latency',
-    version='0.0.4',
+    version='0.0.5',
     description='Meassure latency using TCP.',
     long_description=long_description,
     long_description_content_type='text/markdown',
