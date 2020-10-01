279aeb2f87dbf251a038716288b001e83c5170ef
Rascal_Two <therealrascaltwo@gmail.com>
2020-10-01 15:51:09 -0400

    Fix CLI RTT calculation not including last latency point
    Signed-off-by: Rascal_Two <therealrascaltwo@gmail.com>

--- tcp_latency/tcp_latency.py
+++ tcp_latency/tcp_latency.py
@@ -76,6 +76,8 @@ def measure_latency(
         last_latency_point = latency_point(
             host=host, port=port, timeout=timeout,
         )
+        latency_points.append(last_latency_point)
+
         if human_output:
             if i == 0:
                 print('tcp-latency {}'.format(host))
@@ -91,8 +93,6 @@ def measure_latency(
                     print(
                         f'rtt min/avg/max = {min(received_points)}/{mean(received_points)}/{max(received_points)} ms',   # noqa: E501
                     )
-
-        latency_points.append(last_latency_point)
 
     return latency_points
 
